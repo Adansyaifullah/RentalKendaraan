93503271b5c57048cbb99bef6569c7e4ae88eaa7
AdanaSyaifullah <datebayo200116@gmail.com>
2021-12-14 11:32:16 +0700

    Add project files.

---
+++ RentalKendaraan/Controllers/JenisKendaraansController.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RentalKendaraan.Models;
+
+namespace RentalKendaraan.Controllers
+{
+    public class JenisKendaraansController : Controller
+    {
+        private readonly RentKendaraanContext _context;
+
+        public JenisKendaraansController(RentKendaraanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: JenisKendaraans
+        public async Task<IActionResult> Index(string cstmr, string searchString, string currentFilter, int? pageNumber, string sortOrder)
+        {
+
+            //membuat pagedlist
+            ViewData["CurrentSort"] = sortOrder;
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+            //buat list menyimpan ketersediaan
+            var cstmrList = new List<string>();
+            //query mengambil data
+            var cstmrQuery = from d in _context.JenisKendaraan orderby d.NamaJenisKendaraan select d.NamaJenisKendaraan;
+
+            cstmrList.AddRange(cstmrQuery.Distinct());
+
+            //untuk nampilkan view
+            ViewBag.cstmr = new SelectList(cstmrList);
+
+            //panggil db context
+            var menu = from m in _context.JenisKendaraan select m;
+
+            //untuk memilih dropdownlist ketersediaan
+            if (!string.IsNullOrEmpty(cstmr))
+            {
+                menu = menu.Where(x => x.NamaJenisKendaraan == cstmr);
+            }
+
+            //untuk search data
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                menu = menu.Where(s => s.NamaJenisKendaraan.Contains(searchString));
+            }
+            //untuk sorting
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    menu = menu.OrderByDescending(s => s.NamaJenisKendaraan);
+                    break;
+                default: //name ascending
+                    menu = menu.OrderBy(s => s.NamaJenisKendaraan);
+                    break;
+            }
+
+
+            //definisi jumlah data pada halaman
+            int pageSize = 5;
+
+            return View(await PaginatedList<JenisKendaraan>.CreateAsync(menu.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+
+        // GET: JenisKendaraans/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jenisKendaraan = await _context.JenisKendaraan
+                .FirstOrDefaultAsync(m => m.IdJenisKendaraan == id);
+            if (jenisKendaraan == null)
+            {
+                return NotFound();
+            }
+
+            return View(jenisKendaraan);
+        }
+
+        // GET: JenisKendaraans/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: JenisKendaraans/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdJenisKendaraan,NamaJenisKendaraan")] JenisKendaraan jenisKendaraan)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jenisKendaraan);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jenisKendaraan);
+        }
+
+        // GET: JenisKendaraans/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jenisKendaraan = await _context.JenisKendaraan.FindAsync(id);
+            if (jenisKendaraan == null)
+            {
+                return NotFound();
+            }
+            return View(jenisKendaraan);
+        }
+
+        // POST: JenisKendaraans/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdJenisKendaraan,NamaJenisKendaraan")] JenisKendaraan jenisKendaraan)
+        {
+            if (id != jenisKendaraan.IdJenisKendaraan)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jenisKendaraan);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JenisKendaraanExists(jenisKendaraan.IdJenisKendaraan))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jenisKendaraan);
+        }
+
+        // GET: JenisKendaraans/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jenisKendaraan = await _context.JenisKendaraan
+                .FirstOrDefaultAsync(m => m.IdJenisKendaraan == id);
+            if (jenisKendaraan == null)
+            {
+                return NotFound();
+            }
+
+            return View(jenisKendaraan);
+        }
+
+        // POST: JenisKendaraans/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var jenisKendaraan = await _context.JenisKendaraan.FindAsync(id);
+            _context.JenisKendaraan.Remove(jenisKendaraan);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JenisKendaraanExists(int id)
+        {
+            return _context.JenisKendaraan.Any(e => e.IdJenisKendaraan == id);
+        }
+    }
+}
---
+++ RentalKendaraan/Models/Kendaraan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan.Models
+{
+    public partial class Kendaraan
+    {
+        public Kendaraan()
+        {
+            Peminjaman = new HashSet<Peminjaman>();
+        }
+
+        public int IdKendaraan { get; set; }
+        [Required(ErrorMessage = "Nama Kendaraan tidak boleh kosong")]
+        public string NamaKendaraan { get; set; }
+        [MinLength(6, ErrorMessage = "No Polisi minimal 6 angka")]
+        [MaxLength(11, ErrorMessage = "No Polisi maksimal 11 angka")]
+        public string NoPolisi { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya boleh diisi oleh angka")]
+        public string NoStnk { get; set; }
+        public int? IdJenisKendaraan { get; set; }
+        public string Ketersediaan { get; set; }
+
+        public JenisKendaraan IdJenisKendaraanNavigation { get; set; }
+        public ICollection<Peminjaman> Peminjaman { get; set; }
+    }
+}
